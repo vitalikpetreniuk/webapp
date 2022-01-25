81dac96f460a8300b8dcb48a1b0a87d3f62d7ab2
deggion <deggiondeggion@gmail.com>
2022-01-25 17:06:26 +0200

    new components created

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,67 @@
 
 import flatpickr from "flatpickr";
 import 'flot';
+import monthSelectPlugin from "flatpickr/dist/plugins/monthSelect/index.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	// const $form = $('.drag-drop');
+	const $revenueForm = $('#revenueForm')
+	const $revenueFile = document.querySelector('#revenueFile')
+	const $revenueSelected = document.querySelector('#revenueForm .drag-drop__selected')
+	
+	const $expensesForm = $('#expensesForm')
+	const $expensesFile = document.querySelector('#expensesFile')
+	const $expensesSelected = document.querySelector('#expensesForm .drag-drop__selected')
+	
+	let droppedFiles = false;
+	
+	function handleChangeFile (form, input, selected) {
+		form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+		})
+			.on('dragover dragenter', function() {
+				form.addClass('is-dragover');
+			})
+			.on('dragleave dragend drop', function() {
+				form.removeClass('is-dragover');
+			})
+			.on('drop', function(e) {
+				console.log('drop')
+				droppedFiles = e.originalEvent.dataTransfer.files;
+				selected.classList.add('active')
+			});
+		
+
+		if (input) {
+			selected.addEventListener('click', function () {
+				input.value = ''
+				selected.classList.remove('active')
+			})
+			
+			input.addEventListener('change', function () {
+				if (this.value) {
+					console.log('файл был выбран', this.value)
+					selected.classList.add('active')
+				} else {
+					selected.classList.remove('active')
+					console.log('Файл не был выбран')
+				}
+			})
+		}
+			
+	}
+
+	handleChangeFile($revenueForm, $revenueFile, $revenueSelected)
+	handleChangeFile($expensesForm, $expensesFile, $expensesSelected)
+
+	
+	
+	
+
 	const datepicker = document.getElementById('datepicker')
+	const datepickerModal = document.getElementById('datepicker-modal')
 
 	$('.mounthly-calc__list .title').on('click', function () {
 		$(this).toggleClass('active')
@@ -17,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	})
 	
-	flatpickr($('#datepicker'), {
+	flatpickr($('#datepicker, #datepicker-modal'), {
 		mode: "range",
 		minDate: "today",
 		dateFormat: "d.m.Y",
@@ -25,34 +82,46 @@ document.addEventListener('DOMContentLoaded', () => {
 		showMonths: 3,
 	})
 	
-	if (datepicker.value.length === 0) {
-		$('.datepicker__icon').css({
-				'left': '0'
+	flatpickr($('#monthpicker'), {
+		defaultDate: new Date(),
+		plugins: [
+			new monthSelectPlugin({
+				shorthand: true, //defaults to false
+				dateFormat: "m.y", //defaults to "F Y"
+				altFormat: "F Y", //defaults to "F Y"
+				theme: "dark", // defaults to "light"
 			})
-	} else if (datepicker.value.length <= 10) {
-		$('.datepicker__icon').css({
-			'left': '85px'
-		})
-	} else {
-		$('.datepicker__icon').css({
-			'left': '180px'
-		})
-	}
-	
-	datepicker.oninput = function () {
-		let lengthInput = datepicker.value.length
-		
-		if (lengthInput <= 10) {
-			$('.datepicker__icon').css({
-				'left': '85px'
-			})
-		} else {
-			$('.datepicker__icon').css({
-				'left': '180px'
-			})
-		}
-		datepicker.value = datepicker.value.replace('to', '-')
-	}
+		]
+	})
+
+	// if (datepicker.value.length === 0) {
+	// 	$(`.datepicker .datepicker__icon`).css({
+	// 		'left': '0'
+	// 	})
+	// } else if (datepicker.value.length <= 10) {
+	// 	$(`.datepicker .datepicker__icon`).css({
+	// 		'left': '85px'
+	// 	})
+	// } else {
+	// 	$(`.datepicker .datepicker__icon`).css({
+	// 		'left': '180px'
+	// 	})
+	// }
+
+	// datepicker.oninput = function () {
+	// 	let lengthInput = datepicker.value.length
+	//
+	// 	if (lengthInput <= 10) {
+	// 		$(`.datepicker .datepicker__icon`).css({
+	// 			'left': '85px'
+	// 		})
+	// 	} else {
+	// 		$(`.datepicker .datepicker__icon`).css({
+	// 			'left': '180px'
+	// 		})
+	// 	}
+	// 	datepicker.value = datepicker.value.replace('to', '-')
+	// }
 	
 	$('#revenue').click(() => {
 		$('#modal-revenue, .modal-overlay').addClass('active')
