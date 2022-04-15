6d39e94c4e30cc45f6c10559f6f1d520965c9401
vitaliy <petreniuk.ua@gmail.com>
2022-04-15 14:50:53 +0300

    fix hover error

--- app/js/app.js
+++ app/js/app.js
@@ -17,104 +17,72 @@ jQuery(function ($) {
 	const $expensesSelected = document.querySelector('#expensesForm .drag-drop__selected')
 	const $expensesTextLabel = document.querySelector('#expensesForm .drag-drop__selected .filename')
 
-
-
-
 	if ($('.autoComplete').length) {
-		const autoCompleteJS = new autoComplete({
-			selector: "#autoFill",
-			data: {
-				src: async () => {
-					try {
-						// Loading placeholder text
-						document
-							.getElementById("autoFill")
-							.setAttribute("placeholder", "Loading...");
-						// Fetch External Data Source
-						const source = await fetch(
-							"https://tarekraafat.github.io/autoComplete.js/demo/db/generic.json"
-						);
-						const data = await source.json();
-						// Post Loading placeholder text
-						document
-							.getElementById("autoFill")
-							.setAttribute("placeholder", autoCompleteJS.placeHolder);
-						// Returns Fetched data
-						return data;
-					} catch (error) {
-						return error;
-					}
-				},
-				keys: ["food", "cities", "animals"],
-				cache: true,
-				filter: (list) => {
-					// Filter duplicates
-					// incase of multiple data keys usage
-					const filteredResults = Array.from(
-						new Set(list.map((value) => value.match))
-					).map((food) => {
-						return list.find((value) => value.match === food);
-					});
-
-					return filteredResults;
+		fetch(window.location.origin + '/api/sources').then(
+			async (source) => {
+				let data;
+				try {
+					data = Object.values(await source.json());
+				}catch (e) {
+					data = {}
 				}
-			},
-			placeHolder: "Source",
-			resultsList: {
-				element: (list, data) => {
-					const info = document.createElement("p");
-					if (data.results.length > 0) {
-						info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
-					} else {
-						info.innerHTML = `Создать категорию <strong>"${data.query}"</strong>`;
-					}
-					list.prepend(info);
-				},
-				noResults: true,
-				maxResults: 15,
-				tabSelect: true
-			},
-			resultItem: {
-				element: (item, data) => {
-					// Modify Results Item Style
-					item.style = "display: flex; justify-content: space-between;";
-					// Modify Results Item Content
-					item.innerHTML = `
+				console.log(data)
+				const autoCompleteJS = new autoComplete({
+					data: {
+						src: data,
+						cache: true,
+					},
+					placeHolder: "Source",
+					resultsList: {
+						element: (list, data) => {
+							const info = document.createElement("p");
+							if (data.results.length > 0) {
+								info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
+							} else {
+								info.innerHTML = `Создать категорию <strong>"${data.query}"</strong>`;
+							}
+							list.prepend(info);
+						},
+						noResults: true,
+						maxResults: 15,
+						tabSelect: true
+					},
+					resultItem: {
+						element: (item, data) => {
+							// Modify Results Item Style
+							item.style = "display: flex; justify-content: space-between;";
+							// Modify Results Item Content
+							item.innerHTML = `
       <span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
         ${data.match}
-      </span>
-      <span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
-        ${data.key}
       </span>`;
-				},
-				highlight: true
-			},
-			events: {
-				input: {
-					focus: () => {
-						if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+						},
+						highlight: true
+					},
+					events: {
+						input: {
+							focus: () => {
+								if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+							}
+						}
 					}
-				}
+				});
+				autoCompleteJS.input.addEventListener("selection", function (event) {
+					const feedback = event.detail;
+					autoCompleteJS.input.blur();
+					// Prepare User's Selected Value
+					const selection = feedback.selection.value;
+					// Render selected choice to selection div
+					document.querySelector(".selection").innerHTML = selection;
+					// Replace Input value with the selected value
+					autoCompleteJS.input.value = selection;
+					// Console log autoComplete data feedback
+					console.log(feedback);
+				});
 			}
-		});
-
-
-
-		autoCompleteJS.input.addEventListener("selection", function (event) {
-			const feedback = event.detail;
-			autoCompleteJS.input.blur();
-			// Prepare User's Selected Value
-			const selection = feedback.selection.value[feedback.selection.key];
-			// Render selected choice to selection div
-			document.querySelector(".selection").innerHTML = selection;
-			// Replace Input value with the selected value
-			autoCompleteJS.input.value = selection;
-			// Console log autoComplete data feedback
-			console.log(feedback);
-		});
+		)
 	}
-		
-		
+
 	const months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.',]
 	let $listMonths = document.querySelectorAll('#listMonths li')
 	
