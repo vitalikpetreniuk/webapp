c4679b520c7d9f3c3c800c5a3c9756d28052a551
vitaliy <petreniuk.ua@gmail.com>
2022-04-15 15:07:51 +0300

    fix hover error

--- app/js/app.js
+++ app/js/app.js
@@ -11,14 +11,14 @@ jQuery(function ($) {
 	const $revenueFile = document.querySelector('#revenueFile')
 	const $revenueSelected = document.querySelector('#revenueForm .drag-drop__selected')
 	const $revenueTextLabel = document.querySelector('#revenueForm .drag-drop__selected .filename')
-	
+
 	const $expensesForm = $('#expensesForm')
 	const $expensesFile = document.querySelector('#expensesFile')
 	const $expensesSelected = document.querySelector('#expensesForm .drag-drop__selected')
 	const $expensesTextLabel = document.querySelector('#expensesForm .drag-drop__selected .filename')
 
 	if ($('.autoComplete').length) {
-		fetch(window.location.origin + '/api/sources').then(
+		fetch('https://webapp-bb2am.ondigitalocean.app/api/sources').then(
 			async (source) => {
 				let data;
 				try {
@@ -56,58 +56,58 @@ jQuery(function ($) {
       <span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
         ${data.match}
       </span>`;
+							},
+							highlight: true
 						},
-						highlight: true
-					},
-					events: {
-						input: {
-							focus: () => {
-								if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+						events: {
+							input: {
+								focus: () => {
+									if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+								}
 							}
 						}
-					}
-				});
-				autoCompleteJS.input.addEventListener("selection", function (event) {
-					const feedback = event.detail;
-					autoCompleteJS.input.blur();
-					// Prepare User's Selected Value
-					const selection = feedback.selection.value;
-					// Render selected choice to selection div
-					document.querySelector(".selection").innerHTML = selection;
-					// Replace Input value with the selected value
-					autoCompleteJS.input.value = selection;
-					// Console log autoComplete data feedback
-					console.log(feedback);
-				});
-			}
-		)
-	}
+					});
+					autoCompleteJS.input.addEventListener("selection", function (event) {
+						const feedback = event.detail;
+						autoCompleteJS.input.blur();
+						// Prepare User's Selected Value
+						const selection = feedback.selection.value;
+						// Render selected choice to selection div
+						document.querySelector(".selection").innerHTML = selection;
+						// Replace Input value with the selected value
+						autoCompleteJS.input.value = selection;
+						// Console log autoComplete data feedback
+						console.log(feedback);
+					});
+				}
+			)
+		}
 
 	const months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.',]
 	let $listMonths = document.querySelectorAll('#listMonths li')
-	
+
 	$listMonths.forEach((month, idx) => {
 		month.textContent = months[idx]
 	})
-	
-	
+
+
 	let droppedFiles = false;
-	
-	function handleChangeFile (form, input, selected, textLabel) {
-		form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+
+	function handleChangeFile(form, input, selected, textLabel) {
+		form.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
 			e.preventDefault();
 			e.stopPropagation();
 		})
-			.on('dragover dragenter', function() {
+			.on('dragover dragenter', function () {
 				form.addClass('is-dragover');
 			})
-			.on('dragleave dragend drop', function() {
+			.on('dragleave dragend drop', function () {
 				form.removeClass('is-dragover');
 			})
-			.on('drop', function(e) {
+			.on('drop', function (e) {
 				console.log('drop')
 				droppedFiles = e.originalEvent.dataTransfer.files;
-				
+
 				if (droppedFiles) {
 					console.log('файл был выбран', droppedFiles[0].name)
 					selected.classList.add('active')
@@ -116,19 +116,19 @@ jQuery(function ($) {
 					selected.classList.remove('active')
 					console.log('Файл не был выбран')
 				}
-				
+
 				console.log(droppedFiles.length)
 				console.log(input.files)
 				selected.classList.add('active')
 			});
-		
+
 
 		if (input) {
 			selected.addEventListener('click', function () {
 				input.value = ''
 				selected.classList.remove('active')
 			})
-			
+
 			input.addEventListener('change', function () {
 				let filename = $(this).val().replace(/.*\\/, "");
 				if (this.value) {
@@ -142,15 +142,12 @@ jQuery(function ($) {
 				}
 			})
 		}
-			
+
 	}
 
 	handleChangeFile($revenueForm, $revenueFile, $revenueSelected, $revenueTextLabel)
 	handleChangeFile($expensesForm, $expensesFile, $expensesSelected, $expensesTextLabel)
 
-	
-	
-	
 
 	const datepicker = document.getElementById('datepicker')
 	const datepickerModal = document.getElementById('datepicker-modal')
@@ -163,10 +160,7 @@ jQuery(function ($) {
 			$(this).next().removeClass('active')
 		}
 	})
-	
-	
-	
-	
+
 
 	// if (datepicker.value.length === 0) {
 	// 	$(`.datepicker .datepicker__icon`).css({
@@ -187,7 +181,7 @@ jQuery(function ($) {
 			datepicker.value = datepicker.value.replace('to', '-')
 		}
 	}
-	
+
 	$('#revenue').click(() => {
 		$('#modal-revenue, .modal-overlay').addClass('active')
 		$('html, body').addClass('_over-hidden')
@@ -196,15 +190,14 @@ jQuery(function ($) {
 		$('#modal-expenses, .modal-overlay').addClass('active')
 		$('html, body').addClass('_over-hidden')
 	})
-	
-	
+
+
 	$('.modal__close, .modal-overlay').click(() => {
 		$('.modal, .modal-overlay').removeClass('active')
 		$('html, body').removeClass('_over-hidden')
 	})
-	
-	
-	
+
+
 	// select
 
 	$('.select').on('click', '.select__head', function () {
@@ -232,7 +225,7 @@ jQuery(function ($) {
 			$('.select__list').fadeOut();
 		}
 	});
-	
+
 	$('.btn__edit').click(function (event) {
 		event.preventDefault()
 		event.stopPropagation()
@@ -246,11 +239,10 @@ jQuery(function ($) {
 			$('html, body').removeClass('_over-hidden')
 		})
 	})
-	
+
 	$('form').on('submit', function (e) {
 		e.preventDefault()
 	})
-	
-	
+
 
 })
